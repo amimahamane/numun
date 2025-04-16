c5486a5418787be337fd8efc2f2be29de059a2ed
amimahamane <thisismemphismalik@gmail.com>
2025-04-16 23:05:01 +0000

    Debug Window Re-Size SystemKK

---
+++ gui/components/grid/grid.py
@@ -0,0 +1,10 @@
+from kivy.lang import Builder
+from kivymd.uix.floatlayout import MDFloatLayout
+
+Builder.load_file("components/grid/grid.kv")
+
+
+class Grid(MDFloatLayout):
+    pass
+
+# todo: stopped here
