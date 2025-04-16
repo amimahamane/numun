e9d38f698340da2598ff78c21614ca6bbf92d898
amimahamane <thisismemphismalik@gmail.com>
2025-04-16 19:13:25 +0000

    Add Window Size Handling

--- gui/main.py
+++ gui/main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-
 from kivy.clock import Clock
+from screeninfo import get_monitors
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
@@ -24,10 +24,36 @@ class Main(MDApp):
         return self.manager
 
     def on_start(self):
-        pass
+        monitor = self.get_current_monitor()
+
+        width = monitor["width"]
+        height = monitor["height"]
+
+        Window.size = (width, height)
+        Window.top = 0
+        Window.left = 0
+        Window.fullscreen = True
+
 
     def switch_screen(self, origin, target):
         self.history.append(origin)
         self.manager.current = target
+
+    @staticmethod
+    def get_current_monitor():
+        window_x, window_y = Window.left, Window.top
+
+        for monitor in get_monitors():
+            if (monitor.x <= window_x < monitor.x + monitor.width and
+                    monitor.y <= window_y < monitor.y + monitor.height):
+                return {
+                    "name": monitor.name,
+                    "width": monitor.width,
+                    "height": monitor.height,
+                    "x": monitor.x,
+                    "y": monitor.y
+                }
+
+        return None
 
 Main().run()
